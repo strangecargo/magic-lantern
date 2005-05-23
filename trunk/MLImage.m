64ad0c503236144f8ddc952fdd4d7acc1d4c4996
Allan Hsu <allan@counterpop.net>
2005-05-23 08:55:28 +0000

    - senseless tweaking of build flags
    git-svn-id: file:///home/allan/code/svn/svn/magic-lantern@4 8de0b7c8-c0f7-0310-85b6-c619aef3edd6

--- trunk/MLImage.m
+++ trunk/MLImage.m
@@ -29,7 +29,7 @@
 }
 
 - (void)dealloc {
-	NSLog(@"MLImage dealloc: %@", imagePath);	
+	//NSLog(@"MLImage dealloc: %@", imagePath);	
 	if(image != nil) {
 		[transform release];
 		[imagePath release];
